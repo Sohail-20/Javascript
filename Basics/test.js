786071e51219d0823e39d5ff32956b69f73e4ea6
Sohail-20 <153712079+Sohail-20@users.noreply.github.com>
2023-12-26 06:43:23 +0000

    datatypes

--- Basics/test.js
+++ Basics/test.js
@@ -1,3 +1,15 @@
-console.log("mohammed Sohail")
-console.log("Harsha")
-console.log("Vijay")
+//primitive Datatypes
+let a=5; // number includes decimal values also
+let b="Sohail"; //String
+let c= true; // Boolean
+let d=null; // null and is a standalone value and it is an object
+let e= undefined; // undefined
+let symbol1= Symbol("sohail");// A Symbol is a unique and immutable primitive value and may be used as the key of an Object property (see below). In some programming languages, Symbols are called "atoms". The purpose of symbols is to create unique property keys that are guaranteed not to clash with keys from other code.
+let symbol2= Symbol("sohail");
+console.log(symbol1);
+// User defines datatypes
+
+// 1.Objects
+console.log(typeof e); // o/p=>  undefined
+console.log(typeof d); // o/p=> object
+console.log(typeof symbol1); // o/p=> object
